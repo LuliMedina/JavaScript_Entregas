af1a5e6078ab16aad3830fdf8161c149ad7338d3
LuliMedina <luciamedina7@gmail.com>
2021-03-10 23:33:58 -0300

    33

--- EntregaClase0304/js/main.js
+++ EntregaClase0304/js/main.js
@@ -1,7 +1,13 @@
-let  nombre = prompt("Ingresa tu Nombre");
+let nombre = prompt("Ingresa tu Nombre");
+let apellido = prompt("Ingresa tu Apellido");
+let numeroSecreto = Number(prompt("Ingresa el Número Secreto"));
 
-        let  apellido = prompt("Ingresa tu Apellido");
-    
-        alert("Bienvenido," + " " + nombre + " " + apellido);
-    
-        console.log("Alumno:" + " " + nombre + " " + apellido);
+if (nombre !== "" && apellido !== "" && numeroSecreto === 33) {
+	alert(`Hola, ${nombre}! Haz pasado la prueba de identidad!`);
+} else if (nombre == "" && apellido == "") {
+	alert("Hola! Debes ingresar tu nombre o apellido");
+} else {
+	alert(
+		"No hemos podido comprobar tu identidad. ¿Estás seguro que ese es el Número Secreto?"
+	);
+}
