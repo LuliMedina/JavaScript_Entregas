8d7f4f7436c9980d6aab805498a302d4bab96692
LuliMedina <luciamedina7@gmail.com>
2021-03-10 22:13:28 -0300

    dale

---
+++ EntregaClase0304/js/main.js
@@ -0,0 +1,7 @@
+let  nombre = prompt("Ingresa tu Nombre");
+
+        let  apellido = prompt("Ingresa tu Apellido");
+    
+        alert("Bienvenido," + " " + nombre + " " + apellido);
+    
+        console.log("Alumno:" + " " + nombre + " " + apellido);
