2f4944a41f153ce9c9d2d3d9a93cc58dae662db9
LuliMedina <luciamedina7@gmail.com>
2021-03-17 20:30:13 -0300

    2030

--- EntregaClase0311/js/main.js
+++ EntregaClase0311/js/main.js
@@ -1,7 +1,35 @@
-function calcularprecio(precio) {
+function coniva(precio) {
 	var precio = Number(prompt("Ingresa el precio"));
 	return precio * 0.21 + precio;
 }
 
-let precioIVA = calcularprecio();
-alert(`El precio con IVA es $${precioIVA}`);
+function descuento(descuento) {
+	var descuento = prompt("¿Tenes un descuento?");
+	switch (descuento) {
+		case "si":
+			return 100;
+			break;
+		case "no":
+			return 0;
+			break;
+		default:
+			return alert("Ingresa si o no");
+	}
+}
+
+function conenvio(envio) {
+	var envio = prompt("¿Vivís en Capital Federal o GBA?");
+	switch (envio) {
+		case "si":
+			return 200;
+			break;
+		case "no":
+			return 400;
+			break;
+		default:
+			return alert("Ingresa si o no");
+	}
+}
+
+let precioFinal = coniva() - descuento() + conenvio();
+alert("El precio total de tu compra es $" + precioFinal);
